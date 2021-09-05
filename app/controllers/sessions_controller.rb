9344a48b8e16e7949780cb6ecfbae8a8855f18e7
Amadou <amadou140589@gmail.com>
2021-09-05 22:32:49 +0100

    session create action added. Fkash message layout added to application template.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # If login is successful
+    else
+      flash.now[:danger] = 'I failed to login'
+      render :new
+    end
+  end
 end
