ed5085d76cfe1b6629ad7584ef2c7dd3b213881a
Amadou <amadou140589@gmail.com>
2021-09-05 23:29:25 +0100

    Session login and register implemented. Session helpers methods added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   def new
   end
 
@@ -6,10 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      flash.now[:success] = 'Connected!'
       redirect_to user_path(user.id)
     else
-      flash.now[:danger] = 'I failed to login'
+      flash.now[:danger] = 'Wrong credentials.!'
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:info] = 'logged out'
+    redirect_to new_session_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/register', to: 'users#new', as: 'register'
   resources :users, only: [:new, :create,  :show]
 end
