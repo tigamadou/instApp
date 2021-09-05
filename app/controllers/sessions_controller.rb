e9a11c00f6c9b4e5b46194376019b5edc98a1b4b
Amadou <amadou140589@gmail.com>
2021-09-05 22:36:43 +0100

    session create action edited. session helpers refactored

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # If login is successful
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'I failed to login'
       render :new
