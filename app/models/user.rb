bed2561aa714d39c3bd3d6e971e1a95d8b967418
Amadou <amadou140589@gmail.com>
2021-09-06 02:29:25 +0100

    user referen added to post.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
+  has_many :posts
 end
