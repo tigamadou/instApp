dc41b5738142360a20f09e889e236e35ba8bf8c1
Amadou <amadou140589@gmail.com>
2021-09-07 21:24:02 +0100

    truncate feature edited to not truncate on show pahe

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   storage :file
-  process :resize_to_limit => [150, 150]
+  process :resize_to_limit => [600, 600]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
